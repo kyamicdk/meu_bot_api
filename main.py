3731a80bcffe748b1592316ee7a61182001fedf3
kyamicdk <clebinda06@gmail.com>
2025-04-10 23:30:27 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+flask
+requests
+from flask import Flask, request, jsonify
+import requests
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "API funcionando!"
+
+@app.route("/bypass", methods=["POST"])
+def bypass():
+    data = request.get_json()
+    link = data.get("link")
+    return jsonify({"status": "ok", "result": f"Link recebido: {link}"})
